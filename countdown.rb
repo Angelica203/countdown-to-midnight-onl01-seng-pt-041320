d8d17e00878cc8512148431c4b866140334de299
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-20 20:57:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,3 @@
-#write your code here
-# def countdown
-  
-# while x < 0
-#   puts "#{"HAPPY NEW YEAR!"} SECOND(S)!"
-# countdown = 9
-# end
-# # "HAPPY NEW YEAR!"
-# # def countdown
-# # end
 def countdown (time)
   while time > 0
     puts "#{time} SECOND(S)!"
